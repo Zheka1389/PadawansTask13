57e000253fc91c3018cf936d4f2414e9de5bc6d8
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-12 17:50:25 +0300

    1

---
+++ Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string _surname;
+        private int _age;
+
+        public Employee()
+        {
+        }
+        public Employee(string surname, int age)
+        {
+            _surname = surname;
+            _age = age;
+        }
+        private string Age()
+        {
+            return _age.ToString();
+        }
+        public void Surname(string surname)
+        {
+            _surname = surname;
+        }
+        public string Info()
+        {
+            string info = $"Surname:{_surname}, Age:{_age}";
+            return info;
+        }
+    }
+}
